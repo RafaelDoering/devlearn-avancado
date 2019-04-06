50cb9be4c5adb2b149c7a354ca7f02dabf7b69f3
RafaelDoering <rafael.doering.soares@usp.br>
2019-04-05 22:08:44 -0300

    aula 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <Router>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/signup">Signup</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
         </header>
-      </div>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/signup" component={Signup}></Route>
+      </Router>
     );
   }
 }
---
+++ src/uclContainer/UclContainer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class UclContainer extends Component {
+  render() {
+    return (
+      <h1>UclContainer</h1>
+    )
+  }
+}
+
+export default UclContainer;

fcd3dc42a40cee0f751ae7b52afdd52bc8999d34
RafaelDoering <rafael.doering.soares@usp.br>
2019-04-05 23:04:04 -0300

    Adicionada as funções de listar, criar e deletar aulas

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,69 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 class Home extends Component {
+  state = {
+    course: '',
+    courses: []
+  };
+
+  componentDidMount() {
+    this.courseLoad();
+  }
+
+  courseLoad = () => {
+    axios
+    .get("http://localhost:3000/api/courses")
+    .then(response => {
+      this.setState({ ...this.state, courses: response.data })
+    })
+    .catch(error => {
+      console.log(error)
+    });
+  }
+
+  courseChange = (e) => {
+    this.setState({ ...this.state, course: e.target.value });
+  }
+  
+  onSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:3000/api/courses', { name: this.state.course })
+      .then((response) => {
+        this.courseLoad();
+      })
+      .catch((error => {
+        console.log(error);
+      }))
+  }
+
+  courseDelete = (courseId) => {
+    axios
+      .delete(`http://localhost:3000/api/courses/${courseId}`)
+      .then((response) => {
+        this.courseLoad();
+      })
+      .catch((error => {
+        console.log(error);
+      }))
+  }
+
   render() {
     return (
-      <h1>Home</h1>
+      <React.Fragment>
+        <h1>Home</h1>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" name="course" onChange={this.courseChange}></input>
+          <button type="submit">Enviar</button>
+        </form>
+        <ul>
+          { this.state.courses.map((course) => {
+            return (<li key={course._id} onClick={() => this.courseDelete(course._id)}>{course.name}</li>)
+          }) }
+        </ul>
+      </React.Fragment>
     )
   }
 }
